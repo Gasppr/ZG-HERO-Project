3e5150baa10522db1882a1016b796c48e1e6cb22
Paulo Roberto Antunes De Carvalho <paulorantunes13@hotmail.com>
2023-09-03 17:45:30 -0300

    Criação da classe Pessoa para herança das classes Empresa e Candidato, e interface para métodos gerais

---
+++ linkertinder/src/IPessoa.groovy
@@ -0,0 +1,7 @@
+interface IPessoa {
+
+     void toString(Pessoa pessoa)
+
+
+
+}
---
+++ linkertinder/src/Pessoa.groovy
@@ -0,0 +1,31 @@
+class Pessoa implements IPessoa {
+
+    String nome
+    String email
+    String estado
+    String CEP
+    String descricao
+    String[] competencias
+    String pais
+
+
+    // é especial da classe empresa String CNPJ
+    // é especial da clase empresa
+
+
+
+    @Override
+    void toString(Pessoa pessoa) {
+
+    }
+}
+
+
+
+/*
+Crie uma classe Pessoa que implementa uma
+interface Pessoa, contendo os métodos obrigatórios
+ para os perfis de (Candidato) (Pessoa Física) e
+ (Empresa) (Pessoa jurídica). Para cada tipo de pessoa,
+ estenda essa classe para manter sempre os atributos gerais;
+* */
